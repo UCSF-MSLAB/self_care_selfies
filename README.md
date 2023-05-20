f68997bdeda481e39573af9b946a233230509e1f
Jim Rowson <jim.rowson@gmail.com>
2023-05-19 19:52:24 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# self_care_selfies
+Self Care Selfies helps MS and other patients gain more insight into their health situation. This repository contains code that analyzes videos captured by the patient and calculates several metrics. These metrics have been shown to correlate well with clinical measures of disease status.
+
+To use this code, place the videos to be analyzed into a directory structure like this:
+
+videos
+  patient1
+    01-01-2023
+      BrushL.mp4
+      BrushR.mp4
+      Talk.mp4
+    01-08-2023
+      BrushL.mp4
+      BrushR.mp4
+      Talk.mp4
+  patient2
+    01-01-2023
+      EatL.mov
+      Button.mov
+      Gait.mov
+    01-08-2023
+      EatL.mov
+      Button.mov
+      Gait.mov
+
+The name of the video file represents the activity being performed 
+by the patient. There are several supported activity types.
+
+* Gait - walking
+* Talk - facial expressions while reading or describing
+* Button - fingers and wrist while buttoning a button
+* Eat - fingers and wrist while eating
+* Brush - fingers and wrist while brushing teeth
+
+Add the suffix L and R for button, eat, and brush activities to
+represent the hand being used.
+
+If you name your video directory 'videos' and are ok having the 
+output being written to 'output.csv' then you can simply execute 
+the self_care_selfies.py code.
+
+This command line will show the usage:
+
+$ self_care_selfies.py --help
+usage: python self_care_selfies.py [<video dir = 'videos'> [<output csv file = 'output.csv'> [<input csv file>]]]
+
+The output is a csv file with metrics for each video.
+
+* activity - name of video
+* hand - Left, Right or Pose
+* landmark - name of landmark, such as left_foot_index
+* participant - id of patient
+* date - date video was taken
+* displacement - distance from start to end of landmark position in frame
+* total_travel - total distance traveled of landmark
+* average_velocity - total_travel / num_frames in video
+* peak_velocity - highest distance traveled between adjacent frames
+* normed_velocity - average velocity / peak velocity
+* velocity_peaks - number of peaks in velocity / num_frames in video
+
+
---
+++ self_care_selfies.py
@@ -0,0 +1,381 @@
+#!/usr/local/opt/python@3.9/bin/python3.9
+
+import csv
+import cv2
+import math
+import mediapipe as mp
+import os
+import sys
+
+mpHands = mp.solutions.hands
+mp_pose = mp.solutions.pose
+mpDraw = mp.solutions.drawing_utils
+pose_drawing_style = mp.solutions.drawing_styles.get_default_pose_landmarks_style()
+
+DETECTION_CONFIDENCE = 0.5
+TRACKING_CONFIDENCE = 0.5
+
+SWITCH_HANDS = {
+	"Left": "Right",
+	"Right": "Left"
+}
+
+poseDict = {
+	0: "nose",
+	1: "left_eye_inner",
+	2: "left_eye",
+	3: "left_eye_outer",
+	4: "right_eye_inner",
+	5: "right_eye",
+	6: "right_eye_outer",
+	7: "left_ear",
+	8: "right_ear",
+	9: "mouth_left",
+	10: "mouth_right",
+	11: "left_shoulder",
+	12: "right_shoulder",
+	13: "left_elbow",
+	14: "right_elbow",
+	15: "left_wrist",
+	16: "right_wrist",
+	17: "left_pinky",
+	18: "right_pinky",
+	19: "left_index",
+	20: "right_index",
+	21: "left_thumb",
+	22: "right_thumb",
+	23: "left_hip",
+	24: "right_hip",
+	25: "left_knee",
+	26: "right_knee",
+	27: "left_ankle",
+	28: "right_ankle",
+	29: "left_heel",
+	30: "right_heel",
+	31: "left_foot_index",
+	32: "right_foot_index"
+}
+
+handDict = {
+	0: "wrist",
+	1: "thumb_cmc",
+	2: "thumb_mcp",
+	3: "thumb_ip",
+	4: "thumb_tip",
+	5: "index_finger_mcp",
+	6: "index_finger_pip",
+	7: "index_finger_dip",
+	8: "index_finger_tip",
+	9: "middle_finger_mcp",
+	10: "middle_finger_pip",
+	11: "middle_finger_dip",
+	12: "middle_finger_tip",
+	13: "ring_finger_mcp",
+	14: "ring_finger_pip",
+	15: "ring_finger_dip",
+	16: "ring_finger_tip",
+	17: "pinky_mcp",
+	18: "pinky_pip",
+	19: "pinky_dip",
+	20: "pinky_tip",
+}
+
+def count_peaks(landmarks, axis=1):
+	num_peaks = 0
+	heading_down = False
+	for i, point1 in enumerate(landmarks[:-1]):
+		cur = point1[axis]
+		next = landmarks[i + 1][axis]
+		if cur == next:
+			continue
+		if cur > next and not heading_down:
+			num_peaks += 1
+		heading_down = cur > next
+	return num_peaks
+
+def count_velocity_peaks(landmarks):
+	velocity = []
+	for i, point1 in enumerate(landmarks[:-1]):
+		point2 = landmarks[i + 1]
+		velocity.append((i, distance(point1, point2)))
+	return count_peaks(velocity)
+
+def distance(point1, point2):
+	sum_of_squares = 0
+	for dim, val in enumerate(point1):
+		delta = val - point2[dim]
+		sum_of_squares += delta*delta
+	return math.sqrt(sum_of_squares)
+
+def displacement(landmark):
+	return distance(landmark[0], landmark[-1])
+
+def total_travel(landmarks):
+	travel = 0
+	for i, point1 in enumerate(landmarks[:-1]):
+		point2 = landmarks[i + 1]
+		travel += distance(point1, point2)
+	return travel
+
+def average_velocity(landmarks):
+	return total_travel(landmarks) / len(landmarks)
+
+def peak_velocity(landmarks):
+	peak_travel = 0
+	for i, point1 in enumerate(landmarks[:-1]):
+		point2 = landmarks[i + 1]
+		travel = distance(point1, point2)
+		if travel > peak_travel:
+			peak_travel = travel
+	return peak_travel
+
+def hand_name(hand):
+	which_hand = hand.classification[0].label
+	which_hand = SWITCH_HANDS[which_hand]
+	return which_hand
+
+def process_pose(frame_num, video_landmark_data, results, img):
+	h, w, _ = img.shape
+	if results.pose_landmarks:
+		for idx, lm in enumerate(results.pose_landmarks.landmark):
+			video_landmark_data["Pose"][idx].append((int(lm.x*w), int(lm.y*h)))
+		mpDraw.draw_landmarks(img, results.pose_landmarks,
+			mp_pose.POSE_CONNECTIONS, landmark_drawing_spec=pose_drawing_style)
+
+def process_hand(frame_num, video_landmark_data, results, img):
+	if not results.multi_handedness:
+		return []
+	h, w, _ = img.shape
+	the_landmarks = results.multi_hand_landmarks
+	if the_landmarks:
+		for hand, landmarks in enumerate(the_landmarks):
+			mpDraw.draw_landmarks(img, landmarks, mpHands.HAND_CONNECTIONS)
+			which_hand = hand_name(results.multi_handedness[hand])
+			for idx, lm in enumerate(landmarks.landmark):
+				video_landmark_data[which_hand][idx].append((int(lm.x*w), int(lm.y*h)))
+
+def process_frame(video_landmark_data, frame_num, 
+		processor, result_processor, img):
+	imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	results = processor.process(imgRGB)
+	result_processor(frame_num, video_landmark_data, results, img)
+	cv2.imshow("Image", img)
+	cv2.waitKey(1)
+
+def process_video(video, processor, result_processor, 
+		start_time_secs, end_time_secs):
+	video_data = {
+		"Left": [],
+		"Right": [],
+		"Pose": []
+	}
+	for landmark_index in range(33):
+		video_data["Left"].append([])
+		video_data["Right"].append([])
+		video_data["Pose"].append([])
+	fps = video.get(cv2.CAP_PROP_FPS )
+	while True:
+		success, img = video.read()
+		if not success:
+			break
+		currentFrame = video.get(cv2.CAP_PROP_POS_FRAMES)
+		# currentSec = video.get(cv2.CAP_PROP_POS_MSEC)
+		currentSec = (currentFrame * fps) / 1000.0
+		if currentSec <= start_time_secs:
+			continue
+		if currentSec > end_time_secs:
+			break
+		process_frame(video_data, currentFrame, 
+			processor, result_processor, img)
+	return video_data
+
+def get_video(participant, activity_date, activity_video, ext, video_dir):
+	video_file_path = os.path.join(video_dir, participant, activity_date, 
+		"{}.{}".format(activity_video, ext))
+	return cv2.VideoCapture(video_file_path)
+
+def compute_metrics(video_data, hand, feature):
+	if not video_data["data"][hand][feature]:
+		return None
+	landmarks = video_data["data"][hand][feature]
+	metrics = {
+		"displacement": displacement(landmarks),
+		"total_travel": total_travel(landmarks),
+		"average_velocity": average_velocity(landmarks),
+		"peak_velocity": peak_velocity(landmarks),
+		"normed_velocity": average_velocity(landmarks) / peak_velocity(landmarks),
+		"velocity_peaks": count_velocity_peaks(landmarks) / len(landmarks)
+	}
+	return metrics
+
+def choose_video_type(activity):
+	activity_lower = activity.lower()
+	if activity_lower.startswith("gait"):
+		processor = mp_pose.Pose(min_detection_confidence=0.5, 
+			min_tracking_confidence=0.5)
+		result_processor = process_pose
+		lm_dict = poseDict
+		# select which features to process for Gait videos
+		# left foot (31) and right foot (32)
+		features = [31, 32]
+	elif activity_lower.startswith("talk"):
+		processor = mp_pose.Pose(min_detection_confidence=0.5, 
+			min_tracking_confidence=0.5)
+		result_processor = process_pose
+		lm_dict = poseDict
+		# select which features to process for Talk videos
+		# left foot (31) and right foot (32)
+		features = [31, 32]
+	else:
+		processor = mpHands.Hands(static_image_mode=False,
+			max_num_hands=2,
+			min_detection_confidence=DETECTION_CONFIDENCE,
+			min_tracking_confidence=TRACKING_CONFIDENCE)
+		result_processor = process_hand
+		lm_dict = handDict
+		# select which features to process for Hand videos
+		# wrist (0) and index finger tip (8)
+		features = [0, 8]
+	return processor, result_processor, lm_dict, features
+
+def process_video_for_participant_date_activity(
+		participant, activity_date, activity, ext,
+		start_time_secs, end_time_secs, video_dir):
+	print("processing {} performing {} on {}".format(
+		participant, activity, activity_date))
+	processor, result_processor, lm_dict, features = choose_video_type(activity)
+	video = get_video(participant, activity_date, activity, ext, video_dir)
+	video_data = {
+		"participant": participant,
+		"activity_date": activity_date,
+		"activity": activity,
+		"lm_dict": lm_dict,
+		"features": features,
+		"data": process_video(video, processor, result_processor,
+			start_time_secs, end_time_secs)
+	}
+	return video_data
+
+def get_video_data_using_csv(csv_file, video_dir):
+	rows = []
+	with open(csv_file, 'r', encoding='utf-8-sig') as csvfile:
+		csvreader = csv.reader(csvfile)
+		for row in csvreader:
+			rows.append(row)
+	all_video_data = []
+	headers = ["activity", "hand", "landmark", "participant", "date", "displacement",
+		"total_travel", "average_velocity", 
+		"peak_velocity", "normed_velocity", "velocity_peaks"]
+	output_rows = []
+	for row in rows[1:]:
+		participant, activity_date, activity, ext = row[:4]
+		start_time_secs, end_time_secs = [float(v) for v in row[4:6]]
+		video_data = process_video_for_participant_date_activity(
+			participant, activity_date, activity, ext,
+			start_time_secs, end_time_secs, video_dir)
+		all_video_data.append(video_data)
+	return all_video_data
+
+def ignore_key(participant, activity_date, activity):
+	return "{}-{}-{}".format(participant, activity_date, activity)
+
+def get_video_data_crawling_dir(video_dir, ignore_set):
+	all_video_data = []
+	for (dirpath, dirnames, filenames) in os.walk(video_dir):
+		for filename in filenames:
+			name, ext = os.path.splitext(filename)
+			ext = ext.lower()[1:]
+			if not (ext == "mov" or ext == "mp4"):
+				continue
+			parents = dirpath.split("/")
+			participant, activity_date = parents[1:]
+			key = ignore_key(participant, activity_date, name)
+			if key in ignore_set:
+				continue
+			video_data = process_video_for_participant_date_activity(
+				participant, activity_date, name, ext,
+				0, 24*60*60, video_dir)
+			all_video_data.append(video_data)
+	return all_video_data
+
+def read_ignore_set(output_file):
+	ignore_set = set()
+	rows = []
+	if os.path.exists(output_file):
+		with open(output_file, 'r', encoding='utf-8-sig') as csvfile:
+			csvreader = csv.reader(csvfile)
+			for row in csvreader:
+				rows.append(row)
+			rows = rows[1:]
+			for row in rows:
+				participant = row[3]
+				activity_date = row[4]
+				activity = row[0]
+				key = ignore_key(participant, activity_date, activity)
+				ignore_set.add(key)
+	return ignore_set, rows
+
+def process_all_videos(csv_file, video_dir, output_file):
+	output_rows = []
+	if csv_file:
+		all_video_data = get_video_data_using_csv(csv_file, video_dir)
+	else:
+		ignore_set, output_rows = read_ignore_set(output_file)
+		all_video_data = get_video_data_crawling_dir(video_dir, ignore_set)
+	headers = ["activity", "hand", "landmark", "participant", "date", "displacement",
+		"total_travel", "average_velocity", 
+		"peak_velocity", "normed_velocity", "velocity_peaks"]
+	for one_video_data in all_video_data:
+		for hand in ["Left", "Right", "Pose"]:
+			for landmark in one_video_data["features"]:
+				metrics = compute_metrics(one_video_data, hand, landmark)
+				if metrics:
+					output_rows.append([
+						one_video_data["activity"],
+						hand, 
+						landmark_to_name(one_video_data["lm_dict"], landmark),
+						one_video_data["participant"],
+						one_video_data["activity_date"],
+						metrics["displacement"], 
+						metrics["total_travel"], 
+						metrics["average_velocity"], 
+						metrics["peak_velocity"], 
+						metrics["normed_velocity"], 
+						metrics["velocity_peaks"] 
+						])
+	output_rows.sort(key=lambda row: "{}={}={}={}={}".format(
+		row[0], row[1], row[2], row[3], row[4]))
+	output_rows.insert(0, headers)
+	with open(output_file, 'w', newline='') as csvfile:
+		my_writer = csv.writer(csvfile, delimiter=',',)
+		my_writer.writerows(output_rows)
+
+
+def arg(args, i, default_value):
+	if i < len(args):
+		return args[i]
+	return default_value
+
+def name_to_landmark(lm_dict, name):
+	for landmark in lm_dict.keys():
+		if lm_dict[landmark] == name:
+			return landmark
+	return None
+
+def landmark_to_name(lm_dict, landmark):
+	if landmark not in lm_dict:
+		return None
+	return lm_dict[landmark]
+
+def main():
+	args = sys.argv[1:]
+	if len(args) == 1 and args[0] == "--help":
+		print("usage: python self_care_selfies.py [<video dir = 'videos'> [<output csv file = 'output.csv'> [<input csv file>]]]")
+		exit()
+	video_dir = arg(args, 0, "videos")
+	output_file = arg(args, 1, "output.csv")
+	csv_file = arg(args, 2, "")
+	process_all_videos(csv_file, video_dir, output_file)
+
+if __name__ == "__main__":
+	main()
